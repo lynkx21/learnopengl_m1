11fca452b2220f9627ce4afe7c190a58e7ae8c4b
lynkx21 <lynkx21@gmail.com>
2022-08-03 10:42:40 +0200

    hello world

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#ifdef __APPLE__
+// Defined before OpenGL and GLUT includes to avoid deprecation messages
+#define GL_SILENCE_DEPRECATION
+#endif
 
-int main() {
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main(int argc, char **argv)
+{
     std::cout << "Hello, World!" << std::endl;
+
+    if (!glfwInit()) return -1;
+
+    std::cout << "GLFW lib has been initialized..." << std::endl;
+
     return 0;
 }
